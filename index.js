653cf0acfb436af3c524dca7470781144597ae8b
Anmol Bhardwaj <101956964+anmoliscool@users.noreply.github.com>
2022-08-14 03:14:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,50 @@ function reload(){
 function maker(){ 
   let x=document.querySelector('input').value;
   console.log(x);
-  document.querySelector('form').style.visibility="hidden";
+  document.getElementById('growing').style.visibility="hidden";
   document.getElementById('tiles').style.visibility="visible";
   var e = document.getElementById('box'); 
   for(let i=0;i<=x-1;i++){
     let tile=document.createElement('div');
     tile.id="row";
-    tile.setAttribute('onclick','play()');
+    tile.setAttribute('onclick','player()');
     tile.innerHTML="Press Me Senpai";
     e.appendChild(tile);
 
   }
   
 }
-const songs=["../audio/sound.mp3","../audio/sound1.mp3"]
-function choose(){
-
+/*const songs=["/audio2/DO.wav","/audio2/RE.wav","/audio2/MI.wav","/audio2/FA.wav","/audio2/SO.wav","/audio2/LA.wav","/audio2/TI.wav"];
+let banned=[];
+function rnd(){
+  return Math.floor(Math.random() * 7);
 }
-let r=Math.floor(Math.random() * 2);
-let song=new Audio(songs[r]);
-function play(){
+r=rnd();
+
+let flag=false;
+function player(){
+  function check(){
+    if(banned.includes(r)==false){
+      console.log(r);
+      banned.push(r);
+      return r;
+    }
+    else{
+      r=rnd();
+      return false;
+    }
+  }
+  while(flag=false){
+    flag=check();
+  }
+  let chosen=flag;
+  let song=new Audio(songs[chosen]);
   song.play();
 }
+*/
+function player(){
+  let song = new Audio("../audio/sound.mp3");
+  song.play()
+}
+
+
